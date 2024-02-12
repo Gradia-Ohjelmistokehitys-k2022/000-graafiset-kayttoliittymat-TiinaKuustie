a005310c2e80714f1c62bbd8e4f7d259468b6adc
Tiina Kuustie <tiina.kuustie@gmail.com>
2024-02-12 18:29:20 +0200

    12 palautus

---
+++ 12_palautus_PaintandMouseJatkoa/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace _11_palautus_PaintandMouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(645, 386);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 1;
+            textBox3.TabStop = false;
+            textBox3.Text = "Tabstop false";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            Paint += MainForm_Paint;
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            MouseDown += MainForm_MouseDown;
+            MouseMove += Form1_MouseMove;
+            MouseUp += Form1_MouseUp;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ContextMenuStrip contextMenuStrip1;
+        private TextBox textBox3;
+    }
+}
---
+++ 12_palautus_PaintandMouseJatkoa/Program.cs
@@ -0,0 +1,19 @@
+using _11_palautus_PaintandMouse;
+
+namespace _12_palautus_PaintandMouseJatkoa
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
